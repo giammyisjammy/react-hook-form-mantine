990c319497e163a29f45c0a8540e3435d7d0e32a
gbado <49064602+giammyisjammy@users.noreply.github.com>
2025-04-14 12:43:19 +0200

    fix(types): update ColorInput component type definitions for better TypeScript support

--- src/ColorInput/ColorInput.tsx
+++ src/ColorInput/ColorInput.tsx
@@ -2,16 +2,25 @@ import {
   type UseControllerProps,
   useController,
   type FieldValues,
+  type FieldPath,
 } from "react-hook-form";
 import {
   ColorInput as $ColorInput,
   type ColorInputProps as $ColorInputProps,
 } from "@mantine/core";
 
-export type ColorInputProps<T extends FieldValues> = UseControllerProps<T> &
+export type ColorInputProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+  TTransformedValues = TFieldValues,
+> = UseControllerProps<TFieldValues, TName, TTransformedValues> &
   Omit<$ColorInputProps, "value" | "defaultValue">;
 
-export function ColorInput<T extends FieldValues>({
+export function ColorInput<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+  TTransformedValues = TFieldValues,
+>({
   name,
   control,
   defaultValue,
@@ -19,11 +28,11 @@ export function ColorInput<T extends FieldValues>({
   shouldUnregister,
   onChange,
   ...props
-}: ColorInputProps<T>) {
+}: ColorInputProps<TFieldValues, TName, TTransformedValues>) {
   const {
     field: { value, onChange: fieldOnChange, ...field },
     fieldState,
-  } = useController<T>({
+  } = useController<TFieldValues, TName, TTransformedValues>({
     name,
     control,
     defaultValue,
